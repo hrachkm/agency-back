304501d6acfefcbc28efb1dceb60b5bd97a4b89f
Hrach <hrachkm@gmail.com>
2026-08-30 07:22:04 -0400

    Incorporar los nuevos modulos al proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { SeedModule } from './seed/seed.module';
+import { PropertyTypesModule } from './property-types/property-types.module';
+import { PropertiesModule } from './properties/properties.module';
 
 @Module({
   imports: [
@@ -35,6 +37,8 @@ import { SeedModule } from './seed/seed.module';
     UsersModule,
     AuthModule,
     SeedModule,
+    PropertyTypesModule,
+    PropertiesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
