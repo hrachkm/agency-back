704b451b2c0d9a9c8b0f7ff5510b542160077cfb
Hrach <hrachkm@gmail.com>
2026-08-30 07:23:20 -0400

    Actualizar la estrategia de JWT

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -6,23 +6,26 @@ import { Request } from 'express';
 
 import config from '@/config';
 import { PayloadAccessToken } from '@/auth/models/token.model';
+import { UsersService } from '@/users/services/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    private usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req?.cookies?.access_token || null;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.jwtAccessSecret, //Cambiar segun conveniencia del proyecto
+    });
+  }
 
-	constructor(@Inject(config.KEY) private configService: ConfigType<typeof config>) {
-		super({
-			jwtFromRequest: ExtractJwt.fromExtractors([
-				(req: Request) => {
-					return req?.cookies?.access_token || null;
-				}
-			]),
-			ignoreExpiration: false,
-			secretOrKey: configService.jwtAccessSecret, //Cambiar segun conveniencia del proyecto
-		});
-	}
-
-	validate(payload: PayloadAccessToken) {
-		return payload;
-	}
+  validate(payload: PayloadAccessToken) {
+    return payload;
+  }
 }
